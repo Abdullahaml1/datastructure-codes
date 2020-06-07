4304be6b1596e9ab75e33a5a4a02b7b05055fd6e
Abdullahaml1 <abdullah_aml@protonmail.com>
2020-06-07 22:50:14 +0200

    adding stl vector speed test which was very effcient

---
+++ stack-linked-based/test_vector.cpp
@@ -0,0 +1,13 @@
+#include <stdint.h>
+#include <vector>
+#include <iostream>
+
+
+int main()
+{
+  std::vector <int64_t> vec;
+  for(uint64_t i=0; i<= 128000000; i++)
+    vec.push_back(i);
+
+  return 0;
+}
