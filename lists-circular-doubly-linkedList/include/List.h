3657d6f33deddf7f870ac97dc9f2b060662bd4e7
Abdullahaml1 <abdullah_aml@protonmail.com>
2021-03-05 00:04:38 +0200

    docs: Adding docs for the private methods

--- lists-circular-doubly-linkedList/include/List.h
+++ lists-circular-doubly-linkedList/include/List.h
@@ -56,10 +56,43 @@ public:
 
 private:
 
+  /**
+   * [getting the minimum steps of wether to loop list forward
+   * (in next direction), or loop backwards (in prev direction)]
+   *
+   * @param index [int value. listSize >= index >= -(listSize +1)
+   * ((Note: we take in account the tail node so  otherwise it will be:
+   * listSize > index >= -listSize ))]
+   *
+   * @param lenght [the size of the list excluding the tailNode]
+   *
+   * @return [-lenght/2 > steps >= lenght/2, if steps +ve (in next direction),
+   * if -ve (in prev direction)]
+   */
   int _get_steps(int index, int lenght);
 
+
+  /**
+   * [getting a pointer to the node by the index]
+   *
+   * @param index [int value. listSize >= index >= -(listSize +1)
+   * ((Note: we take in account the tail node so  otherwise it will be:
+   * listSize > index >= -listSize ))]
+   *
+   * @return [pointer to the desired node,
+   *          nullptr if the node does not exist]
+   */
   Node<T> * _get_node(int index);
 
+
+  /**
+   * [insert new node between prev node, and next, node]
+   * @param new_node [pointer to the new node between prev and next node]
+   * @param prev_node [the prev node of the new node index.
+   * Ex: if new_node_index = 4, the prev_node_index = 3]
+   *
+   * @return [0 if sucess, -1 if -1 error occurred ]
+   */
   int _insert_node(Node<T>* new_node, Node<T>* prev_node);
 
   Node<T> * _head_node_ptr;
